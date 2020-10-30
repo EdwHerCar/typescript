7a734e820065b51b09b184c6e7ea1df522bfe5eb
edwardhercar <64983049+edwardhercar@users.noreply.github.com>
2020-10-30 00:37:52 -0600

    Primer commit

---
+++ typescript/template-literal.ts
@@ -0,0 +1,12 @@
+
+(function(){
+
+const nombre ='Fernando';
+const apellido = 'Herrera';
+const edad = 33;
+
+//const salida = nombre + " " + apellido + " ( " + edad + " )"
+const salida =`${nombre} ${apellido} ( ${edad} ) `
+console.log(salida);
+})();
+
